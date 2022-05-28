87a76b7381fdd6504019a279d6cacb07c92fc6fd
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-27 20:11:06 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-let firstName = "";
-let age = prompt("How old are you?");
-
-function askName() {
-    firstName = prompt("What is your first name?");
-    console.log(firstName);
-    document.write("Hello " + firstName + ", how goes it?");
-}
-
 function ratePage(){
     let userNumber = prompt("How many stars would you rate my page? 1-5");
     for (let i = 1; i <= userNumber; i++){
