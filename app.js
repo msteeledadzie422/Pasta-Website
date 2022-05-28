f1d017331563c53be6e09806babb0af8354e68f0
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-27 20:33:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+let userName = prompt("What is your name?");
+console.log(userName);
+document.write("Hello " + userName);
 
+function myFunction() {
+  
+  confirm("Click OK if your positive POSITIVE");
+  
+}
