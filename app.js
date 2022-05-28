83182d8f00b8b1d3f5616fa7b56ae118d0fd5146
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-27 20:32:21 -0600

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-let userName = prompt("What is your name?");
-console.log(userName);
-document.write("Hello " + userName);
-
-function myFunction() {
-  
-  confirm("Click OK if your positive POSITIVE");
-  
-}
