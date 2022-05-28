1d5241ac249c2ec4e77460ae8541830b7d6efb14
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-27 20:31:37 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,3 @@ function myFunction() {
   confirm("Click OK if your positive POSITIVE");
   
 }
-
-function displayRating() {
-
-    let output = '';
-    let rating = prompt('scale of 1-5, how many stars?');
-    for(let i = 0; i < rating; i++){
-        output += "<img class='star' src='images/star.png'/>";
-    }
-
-    return document.write(output);
-}
