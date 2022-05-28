7a67deb633993778515cc9421aeaa550359c42ed
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-27 20:19:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+let userName = prompt("What is your name?");
+console.log(userName);
+document.write("Hello " + userName);
+
+function myFunction() {
+  
+  confirm("Click OK if your positive POSITIVE");
+  
+}
+
 function displayRating() {
 
     let output = '';
