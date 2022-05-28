af100f3f2fc448ebbe9efb31eb3aa6f5459f7110
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-27 20:34:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ function myFunction() {
   confirm("Click OK if your positive POSITIVE");
   
 }
+
+function ratePage(){
+    let userNumber = prompt("How many stars would you rate my page? 1-5");
+    for (let i = 1; i <= userNumber; i++){
+        document.write("<img class='star-pic' src='star.jpeg' />");
+    }
