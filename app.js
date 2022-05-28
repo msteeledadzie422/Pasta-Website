af25f06d984d18b788b35e52a0c63c05982f879d
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-27 20:13:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+let userName = prompt("What is your name?");
+console.log(userName);
+document.write("Hello " + userName);
+
+function myFunction() {
+  
+  confirm("Click OK if your positive POSITIVE");
+  
+}
+
+
 function ratePage(){
     let userNumber = prompt("How many stars would you rate my page? 1-5");
     for (let i = 1; i <= userNumber; i++){
