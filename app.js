a5950f1e92d52df90cd59d83917295a931104433
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-27 20:05:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ function askName() {
     console.log(firstName);
     document.write("Hello " + firstName + ", how goes it?");
 }
+
+function ratePage(){
+    let userNumber = prompt("How many stars would you rate my page? 1-5");
+    for (let i = 1; i <= userNumber; i++){
+        document.write("<img class='star-pic' src='star.jpeg' />");
+    }
