bfdf91b01297c1e8a4554fb7bafb6f72309c8fc3
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-30 00:59:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -12,18 +12,16 @@ function ratePage(){
     let userRating = prompt("How many stars would you rate my page? 1-5");
     let message = "";
   
-    for (let i = 1; i <= userRating; i++) {
-        document.write("<img class='star-pic' src='star.jpeg' />");
-  
     if (userRating<=5 && userRating >=1) {
       message = "Thank you for helping us to improve our service - your rating is appreciated";
       alert(message);
+      for (let i = 1; i <= userRating; i++) {
+        document.write("<img class='star-pic' src='star.jpeg' />");
     }
       
-    if (userRating>5) {
+    else (userRating>5) {
       message = "Sorry, we only accept ratings up to 5, but we appreciate your enthusiasm!";
       alert(message);
-    }
     }
   
 }
