5bc1b5c1811cf674cca84179e47f3ff0c2c761ff
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-31 18:22:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ function yourName() {
     for (let rating = 1; rating <= userRating; rating++) {
       document.write("<img class='star-pic' src='star.jpg' />");
     }    
-    else {
+   } else {
     message = "Not a valid entry, please enter a number 1-5, thank you!";
     alert(message);
     }
   }
 
+yourName()
+ratePage()
