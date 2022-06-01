52182665960a8f797f3bc815e45818abef1aeb5b
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-31 18:39:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ function yourName() {
  function ratePage(){
   let userRating = prompt("How many stars would you rate my page? Only 1-5");
   let message = "";
-  
-  if(userRating<=5 && userRating>=1){
+ while(userRating>5 || userRating<1) {
+    message = "Not a valid entry, please enter a number 1-5, thank you!";
+    alert(message);
+    userRating = prompt("How many stars would you rate my page? Only 1-5"); 
+ }
     message = "Thank you!";
     alert(message);
   let images = ""
@@ -16,10 +19,6 @@ function yourName() {
       images += "<img class='star-pic' src='star.jpeg' />");
     }    
     document.write(images)
-   } else {
-    message = "Not a valid entry, please enter a number 1-5, thank you!";
-    alert(message);
-    }
   }
 
 yourName()
