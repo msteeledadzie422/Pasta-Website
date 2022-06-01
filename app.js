6fcbbe71850920aea319e750d661e32032d1fcc0
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-31 18:50:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 function yourName() {
       let userName = prompt('What is your name?');
       console.log(userName);
-  return document.write("Hello " + userName);
+      return document.write("Hello " + userName);
 }
 
  function ratePage(){
-  let userRating = prompt("How many stars would you rate my page? Only 1-5");
-  let message = "";
- while(userRating>5 || userRating<1) {
-    message = "Not a valid entry, please enter a number 1-5, thank you!";
-    alert(message);
-    userRating = prompt("How many stars would you rate my page? Only 1-5"); 
- }
-  if(userRating<=5 && userRating>1) { 
-       message = "Thank you!";
-    alert(message);
-  let images = ""
-    for (let rating = 1; rating <= userRating; rating++) {
-      images += "<img class='star-pic' src='star.jpeg' />");
-    }    
-    document.write(images)
-  }
+      let userRating = prompt("How many stars would you rate my page? Only 1-5");
+      let message = "";
+      while(userRating>5 || userRating<1) {
+            message = "Not a valid entry, please enter a number 1-5, thank you!";
+            alert(message);
+            userRating = prompt("How many stars would you rate my page? Only 1-5"); 
+      }
+      if(userRating<=5 && userRating>1) { 
+            message = "Thank you!";
+            alert(message);
+            let images = ""
+            for (let rating = 1; rating <= userRating; rating++) {
+                  images += "<img class='star-pic' src='star.jpeg' />");
+            }    
+            document.write(images)
+      }
   }
 
 yourName()
