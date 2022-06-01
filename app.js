662a8d8cc3241136c0071ddfa2bac562232b86d5
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-05-31 18:17:59 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function yourName() {
   
     for (let rating = 1; rating <= userRating; rating++) {
       document.write("<img class='star-pic' src='star.jpg' />");
-          
+    }    
     else {
     message = "Not a valid entry, please enter a number 1-5, thank you!";
     alert(message);
